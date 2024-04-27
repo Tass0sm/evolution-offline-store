20746fb4024e790bc3a9743621ddbae3847e738c
Tassos Manganaris <tassos.manganaris@gmail.com>
2024-04-26 23:16:35 -0400

    Remove context menu item

--- src/m-mail-ui.c
+++ src/m-mail-ui.c
@@ -34,68 +34,6 @@
 
 #define REQUIRE_SERVICE_PROTOCOL "maildir"
 
-static gboolean
-get_store_from_folder_tree (EShellView *shell_view,
-			    gchar **pfolder_path,
-			    CamelStore **pstore)
-{
-	EShellSidebar *shell_sidebar;
-	EMFolderTree *folder_tree;
-	gchar *selected_path = NULL;
-	CamelStore *selected_store = NULL;
-	gboolean found = FALSE;
-
-	/* Get hold of Folder Tree */
-	shell_sidebar = e_shell_view_get_shell_sidebar (shell_view);
-	g_object_get (shell_sidebar, "folder-tree", &folder_tree, NULL);
-	if (em_folder_tree_get_selected (folder_tree, &selected_store, &selected_path) ||
-	    em_folder_tree_store_root_selected (folder_tree, &selected_store)) {
-		if (selected_store) {
-			CamelProvider *provider = camel_service_get_provider (CAMEL_SERVICE (selected_store));
-
-			if (provider && g_ascii_strcasecmp (provider->protocol, REQUIRE_SERVICE_PROTOCOL) == 0) {
-				found = TRUE;
-
-				if (pstore)
-					*pstore = g_object_ref (selected_store);
-
-				if (pfolder_path)
-					*pfolder_path = selected_path;
-				else
-					g_free (selected_path);
-
-				selected_path = NULL;
-			}
-
-			g_object_unref (selected_store);
-		}
-
-		g_free (selected_path);
-	}
-
-	g_object_unref (folder_tree);
-
-	return found;
-}
-
-static void
-action_mail_folder_cb (GtkAction *action,
-		       EShellView *shell_view)
-{
-	gchar *folder_path = NULL;
-	CamelStore *store = NULL;
-
-	g_return_if_fail (E_IS_SHELL_VIEW (shell_view));
-
-	if (!get_store_from_folder_tree (shell_view, &folder_path, &store))
-		return;
-
-	g_print ("%s: Executed on folder '%s : %s'\n", G_STRFUNC, camel_service_get_display_name (CAMEL_SERVICE (store)), folder_path);
-
-	g_free (folder_path);
-	g_clear_object (&store);
-}
-
 static void
 action_mail_message_cb (GtkAction *action,
 			EShellView *shell_view)
@@ -127,15 +65,6 @@ action_mail_message_cb (GtkAction *action,
 		g_ptr_array_unref (selected_uids);
 	g_clear_object (&folder);
 }
-
-static GtkActionEntry mail_folder_context_entries[] = {
-	{ "my-mail-ui-folder-action",
-	  "folder-new",
-	  N_("M_y Maildir Folder Action..."),
-	  NULL,
-	  N_("My Maildir Folder Action"),
-	  G_CALLBACK (action_mail_folder_cb) }
-};
 
 static GtkActionEntry mail_message_menu_entries[] = {
 	{ "my-mail-ui-message-action",
@@ -195,7 +124,6 @@ m_mail_ui_update_actions_cb (EShellView *shell_view,
 	ui_manager = e_shell_window_get_ui_manager (shell_window);
 	action_group = e_lookup_action_group (ui_manager, "mail");
 
-	m_utils_enable_actions (action_group, mail_folder_context_entries, G_N_ELEMENTS (mail_folder_context_entries), folder_node);
 	m_utils_enable_actions (action_group, mail_message_menu_entries, G_N_ELEMENTS (mail_message_menu_entries), has_message);
 }
 
@@ -214,12 +142,7 @@ m_mail_ui_init (GtkUIManager *ui_manager,
 		"    </menu>\n"
 		"  </placeholder>\n"
 		"</menubar>\n"
-		"\n"
-		"<popup name=\"mail-folder-popup\">\n"
-		"  <placeholder name=\"mail-folder-popup-actions\">\n"
-		"    <menuitem action=\"my-mail-ui-folder-action\"/>\n"
-		"  </placeholder>\n"
-		"</popup>\n";
+		"\n";
 
 	EShellWindow *shell_window;
 	GtkActionGroup *action_group;
@@ -232,9 +155,6 @@ m_mail_ui_init (GtkUIManager *ui_manager,
 	action_group = e_shell_window_get_action_group (shell_window, "mail");
 
 	/* Add actions to the action group. */
-	e_action_group_add_actions_localized (
-		action_group, GETTEXT_PACKAGE,
-		mail_folder_context_entries, G_N_ELEMENTS (mail_folder_context_entries), shell_view);
 	e_action_group_add_actions_localized (
 		action_group, GETTEXT_PACKAGE,
 		mail_message_menu_entries, G_N_ELEMENTS (mail_message_menu_entries), shell_view);
